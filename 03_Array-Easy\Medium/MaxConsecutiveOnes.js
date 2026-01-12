5030857c2bc57c7dc51adf57e3c2403b9bf02f47
Sumitraj Saroj <sumitrajsaroj01@gmail.com>
2026-01-12 10:41:05 +0530

    solve Max Consecutive Ones problem

---
+++ 03_Array-Easy\Medium/MaxConsecutiveOnes.js
@@ -0,0 +1,13 @@
+var findMaxConsecutiveOnes = function(nums) {
+    let currentCount = 0;
+    let maxCount = 0;
+    for(let i =0; i<nums.length;i++){
+        if(nums[i]==1){
+            currentCount++
+        }else{
+            maxCount = Math.max(currentCount, maxCount)
+            currentCount = 0;
+        }
+    }
+    return Math.max(currentCount, maxCount)
+};
