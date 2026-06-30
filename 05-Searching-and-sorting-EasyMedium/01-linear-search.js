be902979bf708e55096c9b42130def77f10593c9
Sumitraj Saroj <sumitrajsaroj01@gmail.com>
2026-06-30 10:25:11 +0530

    update the linear search code

--- 05-Searching-and-sorting-EasyMedium/01-linear-search.js
+++ 05-Searching-and-sorting-EasyMedium/01-linear-search.js
@@ -1,10 +1,13 @@
-arr = [1, 5, 3, 9, 6, 0, 3, 2];
+const arr = [1, 5, 3, 9, 6, 0, 3, 2];
 
-// Find 0's position
-
-for (let i = 0; i < arr.length; i++) {
-  if (arr[i] == 0) {
-    console.log(i);
-    break;
+function findZeroPosition(array) {
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] === 0) {
+      return i;
+    }
   }
+  return -1;
 }
+
+const position = findZeroPosition(arr);
+console.log("Position of 0 is:", position);
